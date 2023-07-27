a7434e0db7e61fb5e41bb301778414af3bdaf1a1
itsme <itsme@LAPTOP-RJI8500E>
2023-07-27 11:15:39 +0530

    first commit

---
+++ src/com/cg/dayone/Basics.java
@@ -0,0 +1,16 @@
+package com.cg.dayone;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		int a = 14;
+		int b = 10;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+		
+	}
+
+}
